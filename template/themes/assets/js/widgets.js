7f343a74748c6be2d88b1b418ce9b0790047d4b4
Leo Ferrarezi <leoferrarezi@gmail.com>
2024-09-02 09:08:07 -0400

    Template adicionado

---
+++ template/themes/assets/js/widgets.js
@@ -0,0 +1,38 @@
+(function($) {
+  'use strict';
+  $.fn.andSelf = function() {
+    return this.addBack.apply(this, arguments);
+  }
+  $(function() {
+
+    //check all boxes in order status 
+    $("#check-all").click(function () {
+      $(".form-check-input").prop('checked', $(this).prop('checked'));
+    });
+
+    var isrtl = $("body").hasClass("rtl");
+    if ($('.owl-carousel').length) {
+      $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        nav: true,
+        rtl: isrtl,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        navText: ["<i class='mdi mdi-chevron-left'></i>", "<i class='mdi mdi-chevron-right'></i>"],
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
+        }
+      });
+    }
+    });
+})(jQuery);

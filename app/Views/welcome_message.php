4648be0f61d2713d6f1d868591bcbedfb23adb3f
Leo Ferrarezi <leoferrarezi@gmail.com>
2024-09-07 22:52:03 -0400

    testes de modificação

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -224,11 +224,8 @@
     </div>
 
     <div class="heroe">
-
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
-
-        <h2>The small framework with powerful features</h2>
-
+        <h1>Oi!</h1>
+        <h2>Tudo bem? =)</h2>
     </div>
 
 </header>
@@ -237,17 +234,8 @@
 
 <section>
 
-    <h1>About this page</h1>
-
-    <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-    <p>If you would like to edit this page you will find it located at:</p>
-
-    <pre><code>app/Views/welcome_message.php</code></pre>
-
-    <p>The corresponding controller for this page can be found at:</p>
-
-    <pre><code>app/Controllers/Home.php</code></pre>
+    <h1>Calama Devs</h1>
+    <p>Nasce um time pra fazer a diferença</p>
 
 </section>
 

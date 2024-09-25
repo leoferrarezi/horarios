d89c7f10a05c66f9ea579423fabbb592b1af1776
José Claion <joseclaionmartins@gmail.com>
2024-09-25 15:41:50 -0400

    Corrigindo Timestramps

--- app/Models/VersoesModel.php
+++ app/Models/VersoesModel.php
@@ -21,11 +21,11 @@ class VersoesModel extends Model
     protected array $castHandlers = [];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    //protected $deletedField  = 'deleted_at';
 
     // Validation
     protected $validationRules      = [

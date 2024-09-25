2320c412596bd3075e7a2d7945d3d0c45bd0700e
Luis Marcelo <lmfg10@gmail.com>
2024-09-25 11:00:37 -0400

    Implementação da funcionalidade de upload de arquivos EXCEL com validação de extensão, se é XLSX ou XLS

--- app/Controllers/Importacao.php
+++ app/Controllers/Importacao.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
@@ -9,12 +11,30 @@ class Importacao extends BaseController
 {
     public function index()
     {
-        return view(""); // view do formulario de upload aqui 
+
+        return view('');
     }
 
     public function importar_planilha()
     {
-        // 
-        
+
+        $file = $this->request->getFile('');
+
+        if (!$file->isValid()) {
+            return $this->response->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
+                ->setBody('Erro: Arquivo não enviado.');
+        }
+
+        $extension = $file->getClientExtension();
+        if (!in_array($extension, ['xls', 'xlsx'])) {
+            return $this->response->setStatusCode(ResponseInterface::HTTP_UNSUPPORTED_MEDIA_TYPE)
+                ->setBody('Erro: Formato de arquivo não suportado. Apenas XLSX ou XLS');
+        }
+
+        $newFileName = $file->getRandomName();
+        $file->move(WRITEPATH . '', $newFileName);
+
+        return $this->response->setStatusCode(ResponseInterface::HTTP_OK)
+            ->setBody('Arquivo carregado com sucesso.');
     }
 }

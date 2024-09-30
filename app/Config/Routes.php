e4c6e3a3fc82ae2623f8f7b66c7d48b5b8d459c7
Luis Marcelo <lmfg10@gmail.com>
2024-09-30 09:29:15 -0400

    adicao do composer com a biblioteca phpspreadsheet para importacao de planilhas e rotas para importacao.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,8 @@ $routes->get('/sys', 'Home::sys');
 
 //CRUD Professor
 $routes->get('/sys/professor/cadastro', 'Professor::cadastro');
+
+// Rotas importacao 
+$routes->get('/sys/importacao', 'Importacao::index'); 
+$routes->post('/sys/importacao/importar', 'Importacao::importar_planilha'); 
+

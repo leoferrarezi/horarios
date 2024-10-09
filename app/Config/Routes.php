11f93f4b32952b7a5a41c7150cfb152358d363c0
Sanmara Letícia <sanmara.nunes01@gmail.com>
2024-10-09 15:56:26 -0400

    Cadastro de Disciplina Provisório

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,8 @@ $routes->get('/sys', 'Home::sys');
 
 //CRUD Professor
 $routes->get('/sys/professor/cadastro', 'Professor::cadastro');
+$routes->get('/sys/professor/horarios', 'Professor::horarios');
+$routes->get('/sys/professor/confirmar-importacao', 'Professor::validarImportacao');
+$routes->get('/sys/professor/listar-professor', 'Professor::listaProfessor');
+$routes->get('/sys/professor/importar-professor', 'Professor::importarProfessor');
+$routes->get('/sys/disciplina/cadastro', 'Disciplinas::cadastro');

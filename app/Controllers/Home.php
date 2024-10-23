3faa9b152dd45a0c21724f3c23d5639126972cc9
Paloma Carvalho <paloma.clima@gmail.com>
2024-10-22 22:57:37 -0400

    ajuste no home.php, na dashboard.php (com os links de encaminhamento das páginas), criação da página temporária 'em-construcao.php' com msg para o usuário de que a funcionalidade ainda está em fase de implementação.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,12 +11,29 @@ class Home extends BaseController
 
     public function home(): string
     {
-
         $pageContent['title'] = "BEM - VINDO (A).";
 
         $data['content'] = view('sys/home', $pageContent);
 
         return view('dashboard', $data);
-        
+    }
+
+    public function dashboard(): string
+    {
+        // Adicione o conteúdo que deseja exibir na página inicial do dashboard
+        $pageContent['title'] = "Página Inicial do Dashboard";
+
+        $data['content'] = view('sys/dashboard', $pageContent);
+
+        return view('dashboard', $data);
+    }
+
+    public function emConstrucao(): string
+    {
+        $pageContent['title'] = "Página em Construção";
+
+        $data['content'] = view('sys/em-construcao', $pageContent);
+
+        return view('dashboard', $data);
     }
 }
--- app/Controllers/TemposAula.php
+++ app/Controllers/TemposAula.php
@@ -4,11 +4,15 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\TemposAulasModel;
 
 class TemposAula extends BaseController
 {
-    public function index()
+    public function index(): ResponseInterface
     {
-        //
+        $model = new TemposAulasModel();
+        $data = $model->getTemposAula();
+
+        return $this->response->setJSON($data);
     }
 }

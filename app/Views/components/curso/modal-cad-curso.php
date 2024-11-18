7d68247b3d5c6a8544fd60d218cdbb9ce32ba6ee
José Claion <joseclaionmartins@gmail.com>
2024-11-17 23:15:04 -0400

    Ajuste nome modal

--- app/Views/components/curso/modal-cad-curso.php
+++ app/Views/components/curso/modal-cad-curso.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="ModalLabel">Cadastrar Professor</h5>
+            <h5 class="modal-title" id="ModalLabel">Cadastrar Curso</h5>
             <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">×</span>
             </button>

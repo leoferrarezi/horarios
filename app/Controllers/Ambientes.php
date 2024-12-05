e700b5d1f407a5a0a0608651c508b1b5d65ad989
Igor Barreto <igorbarreto1110@gmail.com>
2024-12-05 16:05:23 -0300

    View cadastro de ambientes

--- app/Controllers/Ambientes.php
+++ app/Controllers/Ambientes.php
@@ -3,13 +3,51 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\AmbientesModel;
 
 class Ambientes extends BaseController
 {
     public function index()
     {
-        $data['content'] = view('sys/em-construcao');
+        $ambientesModel = new AmbientesModel();
+        $data['ambientes'] = $ambientesModel->findAll();
+
+        $data['content'] = view('sys/cadastro-ambientes',$data);
         return view('dashboard', $data);
+    }
+
+    public function salvar(){
+        $ambiente = new AmbientesModel();
+
+        $data['ambientes'] = $ambiente->findAll();
+
+        $dadosPost = $this->request->getPost();
+
+        //verifica se já existe um ambiente com o mesmo nome
+        if($ambiente->where('nome', $dadosPost['nome'])->first()){
+            session()->setFlashdata('erro', 'Nome do ambiente já cadastrado');
+            return redirect()->to(base_url('sys/cadastro-ambientes'))->withInput();
+        }
+        
+        if($ambiente->insert($dadosPost)){
+            session()->setFlashdata('sucesso', 'ambiente cadastrado com sucesso.');
+            return redirect()->to(base_url('sys/cadastro-ambientes'));
+
+        } else {
+            $data['erros'] = $ambiente->errors();
+            return redirect()->to(base_url('sys/cadastro-ambientes'))->with('erros', $data['erros'])->withInput();
+        }    
+    }
+
+    public function deletar($id){
+        $ambienteModel = new ambientesModel();
+        
+        // Deletar o ambiente pelo ID
+        if ($ambienteModel->delete($id)) {
+            session()->setFlashdata('sucesso', 'Ambiente deletado com sucesso.');
+            return redirect()->to(base_url('/sys/cadastro-ambientes'));
+        } else {
+            return redirect()->to(base_url('/sys/cadastro-ambientes'))->with('erro', 'Falha ao deletar ambiente');
+        }
     }
 }

202cc7024acf42aa8d8c42925c3af23d3d667cda
Vitória Oliveira <vitorialimaa3007@gmail.com>
2024-12-05 23:08:20 -0400

    Criação da tela de relatórios

---
+++ app/Views/sys/relatorios.php
@@ -0,0 +1,20 @@
+<div class="row"></div>
+<div class="col-md-12 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">RELATÓRIOS</h4>
+            <p class="card-description"></p>
+            <div class="col-md-12 grid-margin stretch-card d-flex justify-content-between">
+                <div class="page-header">
+                    <h3 class="page-title">GERAR RELATÓRIO</h3>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?= base_url('/sys/home') ?>">Início</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Lista Professores</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

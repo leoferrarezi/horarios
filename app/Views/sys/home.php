88b3d0210881364b734cd16fc4635c0307a496c0
Leo Ferrarezi <leoferrarezi@gmail.com>
2024-12-05 23:03:35 -0400

    Correção de importação de scripts js que estavam bugando os menus e itens dinamicos

--- app/Views/sys/home.php
+++ app/Views/sys/home.php
@@ -189,10 +189,3 @@
         </p>
     </div>
 </div>
-<script src="../../../assets/vendors/js/vendor.bundle.base.js"></script>
-<script src="../../../assets/js/off-canvas.js"></script>
-<script src="../../../assets/js/hoverable-collapse.js"></script>
-<script src="../../../assets/js/misc.js"></script>
-<script src="../../../assets/js/settings.js"></script>
-<script src="../../../assets/js/todolist.js"></script>
-<script src="../../../assets/js/tabs.js"></script>

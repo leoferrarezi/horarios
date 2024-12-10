1942f43d44e300ae096161c5288b4e23bd15b5a6
José Claion <joseclaionmartins@gmail.com>
2024-12-09 21:21:31 -0400

    CRUD - Matriz e ajustes

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -81,7 +81,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url('sys/matriz-curricular'); ?>">
+          <a class="nav-link" href="<?php echo base_url('sys/matriz'); ?>">
             <span class="menu-icon">
               <i class="mdi mdi-notebook-check"></i>
             </span>

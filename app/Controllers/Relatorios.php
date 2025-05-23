c568fe68e9064fdfa73f2b617da3e6bc453cf641
Leo Ferrarezi <leoferrarezi@gmail.com>
2025-05-23 14:38:38 -0400

    Mais ajustes pro relatório de ambientes

--- app/Controllers/Relatorios.php
+++ app/Controllers/Relatorios.php
@@ -348,7 +348,7 @@ class Relatorios extends BaseController
             sort($temDias);
             sort($temHorarios);
 
-            if($conta > 0 && $conta < sizeof($tabelas) && sizeof($temHorarios) < 9)
+            if($conta > 0 && $conta < sizeof($tabelas) && sizeof($temHorarios) < 10)
                 $pdf->appendHTML('<div class="page_break"></div>');
 
             $pdf->appendHTML('
